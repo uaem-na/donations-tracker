fde0f44832c47b224305c3d9bee26172ffcd4914
Tristan Stevens <tvut@outlook.com>
2022-02-13 17:32:44 -0500

    added in some random logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import logo from './assets/media/logo.svg';
 import './assets/style/Main.css';
 import Login from './components/login/login';
+import mask from './assets/media/ppe-mask.svg'
 
 function App() {
   return (
-    <div>
-      <Login/>
+    <div className='flex justify-center items-center h-screen'>
+      {/* <Login/> */}
+        Hello UAEM!
+        <img className='mx-8' src={mask}/>
     </div>
   );
 }
