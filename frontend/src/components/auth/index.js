252d2f3491e2e6846b64472bba5ccfafd7edd497
Ryan Seo <shjryan@users.noreply.github.com>
2023-05-29 21:38:27 -0400

    adds templates for front

---
+++ frontend/src/components/auth/index.js
@@ -0,0 +1,3 @@
+export { Login } from "./login";
+export { default as LoginNew } from "./login.new";
+export { default as RegisterNew } from "./register";
---
+++ frontend/src/components/auth/register.new.js
@@ -0,0 +1,9 @@
+const RegisterNew = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+};
+
+export default RegisterNew;
