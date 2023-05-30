7cdf69ec8f3578c21c3693f50a3c1ce7acf93b89
Ryan Seo <shjryan@users.noreply.github.com>
2023-05-29 21:12:51 -0400

    adds layout & global styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,9 +13,8 @@ import Landing from "./components/landing";
 import Userfront from "@userfront/core";
 import ReqOff from "./components/requestOfferPage/reqOff";
 import Dashboard from "./components/dashboard";
-import Navbar from "./components/nav";
-import Login from "./components/auth/login";
-import Register from "./components/auth/register";
+import { Login, Register, LoginNew } from "./components/auth";
+import { Layout } from "./components/layout";
 
 Userfront.init("8nwrppdb");
 
@@ -29,66 +28,75 @@ function RequireAuth({ children }) {
 }
 
 function NoAuth({ children }) {
-  let location = useLocation();
-  if (Userfront.tokens.accessToken) {
-    // Redirect to the /dashboard page
-    return <Navigate to="/profile" state={{ from: location }} replace />;
-  }
+  // let location = useLocation();
+  // if (Userfront.tokens.accessToken) {
+  //   // Redirect to the /dashboard page
+  //   return <Navigate to="/profile" state={{ from: location }} replace />;
+  // }
   return children;
 }
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Router>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/reset" element={<Reset />} />
-          <Route
-            path="/request/:id"
-            element={<ReqOff offer={false} edit={false} />}
-          />
-          <Route
-            path="/offer/:id"
-            element={<ReqOff offer={true} edit={false} />}
-          />
-          <Route
-            path="/new-request"
-            element={<ReqOff offer={false} edit={true} />}
-          />
-          <Route
-            path="/new-offer"
-            element={<ReqOff offer={true} edit={true} />}
-          />
-          <Route
-            path="/login"
-            element={
-              <NoAuth>
-                <Login />
-              </NoAuth>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <NoAuth>
-                <Register />
-              </NoAuth>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <RequireAuth>
-                <Profile />
-              </RequireAuth>
-            }
-          />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Landing />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/reset" element={<Reset />} />
+            <Route
+              path="/request/:id"
+              element={<ReqOff offer={false} edit={false} />}
+            />
+            <Route
+              path="/offer/:id"
+              element={<ReqOff offer={true} edit={false} />}
+            />
+            <Route
+              path="/new-request"
+              element={<ReqOff offer={false} edit={true} />}
+            />
+            <Route
+              path="/new-offer"
+              element={<ReqOff offer={true} edit={true} />}
+            />
+            <Route
+              path="/login"
+              element={
+                <NoAuth>
+                  <Login />
+                </NoAuth>
+              }
+            />
+            <Route
+              path="/login-new"
+              element={
+                <NoAuth>
+                  <LoginNew />
+                </NoAuth>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <NoAuth>
+                  <Register />
+                </NoAuth>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <RequireAuth>
+                  <Profile />
+                </RequireAuth>
+              }
+            />
+          </Route>
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
