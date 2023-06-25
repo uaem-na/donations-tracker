35fa3262fe832c5a5ca31f2f27bf2bb5c77cafdb
Ryan Seo <shjryan@users.noreply.github.com>
2023-06-25 09:15:15 -0400

    remove Main.css and rename app.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./app";
 import configureAppStore from "./app/store";
 import "./assets/style/index-compiled.css";
 
