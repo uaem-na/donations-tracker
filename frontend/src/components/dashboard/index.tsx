a12be179651c042139d3b1bd42e1cb732889788a
Ryan Seo <shjryan@users.noreply.github.com>
2023-07-24 23:14:47 -0400

    renaming

--- frontend/src/components/dashboard/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as OfferLink } from "./OfferLink";
-export { default as Offers } from "./Offers";
-export { default as RequestLink } from "./RequestLink";
-export { default as Requests } from "./Requests";
