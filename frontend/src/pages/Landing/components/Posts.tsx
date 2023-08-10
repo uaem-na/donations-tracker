952c2eee0dd3ffe1fb647c125e1e596c63d043a3
Penley Chan <penleychan@gmail.com>
2023-08-09 21:12:58 -0600

    🚀 perf(app): add client side pagination
    Peformance issues with landing page due to too many posts render in HTML causing lag whenever google map updates the posts, resolve this by implementing client side pagination on the posts that is visible.

--- frontend/src/pages/Landing/components/Posts.tsx
+++ frontend/src/pages/Landing/components/Posts.tsx
@@ -13,6 +13,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PostApiResponse } from "@services/api";
 import { capitalizeFirstLetter } from "@utils";
 import formatDistance from "date-fns/formatDistance";
+import { useEffect, useState } from "react";
 
 interface IPostsProp {
   posts: PostApiResponse[];
@@ -20,6 +21,13 @@ interface IPostsProp {
 }
 
 export const Posts = ({ posts, handleLocateClick }: IPostsProp) => {
+  const postsPerPage = 10;
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [paginatedPosts, setPaginatedPosts] = useState<PostApiResponse[]>();
+
+  const [totalPages, setTotalPages] = useState<number>(0);
+
   const renderStatusIndicator = (status) => {
     switch (status) {
       case "open":
@@ -33,77 +41,128 @@ export const Posts = ({ posts, handleLocateClick }: IPostsProp) => {
     }
   };
 
+  useEffect(() => {
+    if (posts) {
+      const indexOfLastPost = currentPage * postsPerPage;
+      const indexOfFirstPost = indexOfLastPost - postsPerPage;
+      setPaginatedPosts(posts.slice(indexOfFirstPost, indexOfLastPost));
+
+      setTotalPages(Math.ceil(posts.length / postsPerPage));
+    }
+  }, [posts, currentPage]);
+
+  // reset current page whenever there's change
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [posts]);
+
+  const handlePrev = () => {
+    if (currentPage === 1) return;
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
   return (
     <>
-      {posts?.length ? (
-        <ul className="divide-y divide-gray-200">
-          {posts?.map((post) => {
-            return (
-              <li key={post.id} className="relative flex flex-col">
-                <div className="flex items-center space-x-4 py-4">
-                  <div className="min-w-0 flex-auto">
-                    <div className="flex items-center gap-x-3">
-                      <h2 className="min-w-0 text-sm font-semibold leading-6 text-gray-500">
-                        <div className="flex gap-x-2">
-                          <span className="truncate">{post.item.name}</span>
-                        </div>
-                      </h2>
-                    </div>
-                    <div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
-                      <div className="truncate">
-                        <div className="flex justify-center items-center">
-                          {renderStatusIndicator(post.status)}
-                          <span className="ml-1">
-                            {capitalizeFirstLetter(post.status)}
-                          </span>
-                        </div>
+      {paginatedPosts?.length ? (
+        <>
+          <ul className="divide-y divide-gray-200">
+            {paginatedPosts?.slice(0, 10)?.map((post) => {
+              return (
+                <li key={post.id} className="relative flex flex-col">
+                  <div className="flex items-center space-x-4 py-4">
+                    <div className="min-w-0 flex-auto">
+                      <div className="flex items-center gap-x-3">
+                        <h2 className="min-w-0 text-sm font-semibold leading-6 text-gray-500">
+                          <div className="flex gap-x-2">
+                            <span className="truncate">{post.item.name}</span>
+                          </div>
+                        </h2>
                       </div>
-                      &mdash;
-                      <p className="whitespace-nowrap">
-                        {formatDistance(new Date(post.createdAt), new Date())}{" "}
-                        ago
-                      </p>
+                      <div className="mt-3 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
+                        <div className="truncate">
+                          <div className="flex justify-center items-center">
+                            {renderStatusIndicator(post.status)}
+                            <span className="ml-1">
+                              {capitalizeFirstLetter(post.status)}
+                            </span>
+                          </div>
+                        </div>
+                        &mdash;
+                        <p className="whitespace-nowrap">
+                          {formatDistance(new Date(post.createdAt), new Date())}{" "}
+                          ago
+                        </p>
+                      </div>
                     </div>
+                    <Badge
+                      color={post.type === "offer" ? "purple" : "blue"}
+                      text={capitalizeFirstLetter(post.type)}
+                    />
                   </div>
-                  <Badge
-                    color={post.type === "offer" ? "purple" : "blue"}
-                    text={capitalizeFirstLetter(post.type)}
-                  />
-                </div>
-                <div className="flex justify-end gap-x-2 mb-1">
-                  <Drawer>
-                    <DrawerTrigger asChild>
-                      <button
-                        type="button"
-                        className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 cursor-pointer"
-                      >
-                        Details
-                      </button>
-                    </DrawerTrigger>
-                    <DrawerContent size="medium">
-                      <DrawerHeader>
-                        <PostDetails id={post.id} onError={() => {}} />
-                      </DrawerHeader>
-                    </DrawerContent>
-                  </Drawer>
+                  <div className="flex justify-end gap-x-2 mb-1">
+                    <Drawer>
+                      <DrawerTrigger asChild>
+                        <button
+                          type="button"
+                          className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 cursor-pointer"
+                        >
+                          Details
+                        </button>
+                      </DrawerTrigger>
+                      <DrawerContent size="medium">
+                        <DrawerHeader>
+                          <PostDetails id={post.id} onError={() => {}} />
+                        </DrawerHeader>
+                      </DrawerContent>
+                    </Drawer>
 
-                  {handleLocateClick && (
-                    <Tooltip message="Locate" asChild>
-                      <button
-                        type="button"
-                        className="rounded bg-red-500 px-2 py-1 text-xs text-gray-50 shadow-sm ring-1 ring-inset ring-red-600 hover:bg-red-600 cursor-pointer"
-                        onClick={() => handleLocateClick(post)}
-                      >
-                        <FontAwesomeIcon icon={faLocationDot} />
-                        <span className="sr-only">Locate</span>
-                      </button>
-                    </Tooltip>
-                  )}
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                    {handleLocateClick && (
+                      <Tooltip message="Locate" asChild>
+                        <button
+                          type="button"
+                          className="rounded bg-red-500 px-2 py-1 text-xs text-gray-50 shadow-sm ring-1 ring-inset ring-red-600 hover:bg-red-600 cursor-pointer"
+                          onClick={() => handleLocateClick(post)}
+                        >
+                          <FontAwesomeIcon icon={faLocationDot} />
+                          <span className="sr-only">Locate</span>
+                        </button>
+                      </Tooltip>
+                    )}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+          <nav
+            className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6 mt-2"
+            aria-label="Pagination"
+          >
+            <div className="flex flex-1 justify-between sm:justify-end">
+              {currentPage === 1 ? null : (
+                <button
+                  type="button"
+                  onClick={handlePrev}
+                  className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
+                >
+                  Previous
+                </button>
+              )}
+              {currentPage === totalPages ? null : (
+                <button
+                  type="button"
+                  onClick={handleNext}
+                  className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0"
+                >
+                  Next
+                </button>
+              )}
+            </div>
+          </nav>
+        </>
       ) : (
         <div className="flex flex-col items-center justify-center h-full text-center">
           <FontAwesomeIcon
