13a7041387514697c19f6cb1244180d5ec03bbc6
Sophia <44929270+slitechs@users.noreply.github.com>
2023-11-25 11:21:48 -0500

    Updated FAQ page with new content (#108)
    Updated the questions and answers on the FAQ according to the content document.

--- frontend/src/pages/Faq/Faq.tsx
+++ frontend/src/pages/Faq/Faq.tsx
@@ -50,19 +50,94 @@ export const FaqPage = () => {
       <div className="mx-auto max-w-7xl px-6 py-20 lg:px-8">
         <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
           <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
-            Frequently asked questions
+            Frequently Asked Questions
           </h2>
           <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
-            <FaqItem question="How is Donations Trackers different from other product-sharing websites?">
+          <FaqItem question="What is Donations Tracker?">
+              <p className="mb-3 leading-5">
+              Donations Tracker is an online platform aimed at promoting donation sharing in Montréal. On this platform, users can request and offer donations. In doing so, our goal is to strengthen community solidarity and reduce socioeconomic disparities here in Montréal.
+              </p>
+            </FaqItem>
+
+            <FaqItem question="How do I make an offer and/or request for a donation on Donations Tracker?">
+              <p className="mb-3 leading-5">
+              <ol className="list-inside my-5 list-decimal marker:font-semibold">
+                <li className="mb-3 d leading-5">
+                  <span className="font-semibold">REGISTER AS AN USER.</span>
+                  <p className="mb-3 leading-5">
+                    Click on “Login” on the top right then click “Click here to register.” 
+                  </p>
+                  <p className="mb-3 leading-5">
+                    You can register as an individual or an organization. For more information on their differences, please read “What is the difference between registering as an individual vs. organization?” below. 
+                  </p>
+                </li>
+                <li className="mb-3 d">
+                  <span className="font-semibold">MAKE AN OFFER AND/OR REQUEST.</span>
+                  <p className="mb-3 leading-5">
+                    Once you log in, click on “Posts” in the menu bar on the left. Click on “Make an Offer” or “Make a Request” and fill in the appropriate information. 
+                  </p>
+                  <p className="mb-3 leading-5 text-sm">
+                    Note: Only specific users can make a request. For more information, please see “What is the difference between registering as an individual vs. organization?” below.
+                  </p>
+                  <p className="mb-3 leading-5 text-sm">
+                    Note: The current version of the website allows purchasing/selling of products. If you want to offer/request a donation, please input ‘0’ as the price.
+                  </p>
+                </li>
+                <li className="mb-3 d">
+                  <span className="font-semibold">EXPLORE THE “POSTS” PAGE FOR ANY OFFERS/REQUESTS YOU WANT TO RESPOND TO.</span>
+                  <p className="mb-3 leading-5">
+                    Scroll through the offers/requests listed on “Posts.” You might have products another user may need! Or, another user may be offering items that you want! 
+                  </p>
+                  <p className="mb-3 leading-5">
+                    Use the filters on the left to look at specific posts. You can also add certain posts to your “Favorites” by clicking on the star button.
+                  </p>
+                </li>
+                <li className="mb-3 d">
+                  <span className="font-semibold">CLICK ON THE OFFER/REQUEST THAT YOU WANT. CONTACT THE OTHER USER THROUGH EMAIL LISTED ON THE POST.</span>
+                  <p>
+                    All communication/transactions between users must occur outside of the Donations Tracker platform. 
+                  </p>
+                </li>
+                <li className="mb-3 d">
+                <span className="font-semibold">COMMUNICATE WITH THE OTHER USER TO COMPLETE THE TRANSACTION/DONATION.</span>
+                </li>
+                <li className="mb-3 d">
+                  <span className="font-semibold">CONGRATULATIONS! YOUR OFFER/REQUEST HAS BEEN FULFILLED! LASTLY, PLEASE CLOSE YOUR POST ON THE WEBSITE.</span> 
+                  <p>
+                    It is important that you close your completed offers/requests so that other users know that it’s no longer active. We thank you for your cooperation! 
+                  </p>
+                </li>
+              </ol>
+              </p>
+            </FaqItem>
+
+            <FaqItem question="What can I do on the “Account” page?">
+              <p className="mb-3 leading-5">
+                The “Account” page appears for users who have logged in. 
+              </p>
+              <ul className="list-disc list-inside my-5">
+                <li className="mb-3">
+                 Dashboard: Users can view their own posts as well as posts marked as their “Favorite.” 
+                </li>
+                <li className="mb-3">
+                  General: Users can change their first, last, and display names. If you want to change your associated email, please contact the administrators at <ExternalLink href="mailto: uaem@ssmu.ca">uaem@ssmu.ca</ExternalLink>.
+                </li>
+                <li className="mb-3">
+                  Security: Users can change their password. 
+                </li>
+              </ul>
+            </FaqItem>
+
+            <FaqItem question="How is Donations Tracker different from other product-sharing websites?">
               <p className="mb-3 leading-5">
                 Although there is the option to sell/purchase items on this
-                website, Donations Trackers stands out from other
+                website, Donations Tracker stands out from other
                 product-sharing platforms in the sense that it promotes donation
                 sharing. The development team realized that despite there being
                 numerous platforms to sell/purchase products, there is a lack of
                 donation-promoting websites. By making donations more
-                accessible, Donations Trackers hopes to enhance community morale
-                and well-being."
+                accessible, Donations Tracker hopes to enhance community morale
+                and well-being.
               </p>
             </FaqItem>
 
@@ -80,7 +155,10 @@ export const FaqPage = () => {
                 no geographic restraints for individual users (i.e. individual
                 users don't have to be living in the Greater Montréal area).
                 However, individual users can only offer donations/products (not
-                request). Registering as an organization makes it mandatory that
+                request). 
+              </p>
+              <p className="mb-3 leading-5">
+                Registering as an organization makes it mandatory that
                 the user be associated with a legitimate organization. The
                 legitimacy of the organization will be approved by the website
                 administrators during registration. For the purpose of the
@@ -112,7 +190,7 @@ export const FaqPage = () => {
                   If you have reasonable doubt about another user's
                   reliability/legitimacy, you can always report them by clicking
                   on the <Highlight>Report User</Highlight> button, which can be
-                  found on TBD . Once a user has been reported, their activity
+                  found on the _____. Once a user has been reported, their activity
                   will be temporarily blocked until the website administrators
                   conduct a background check.
                 </li>
@@ -155,16 +233,16 @@ export const FaqPage = () => {
                 temporarily blocked. Donations Trackers thanks you for your
                 understanding and cooperation.
               </p>
-              <p className="mb-3 leading-5">
-                <i>
-                  If you have any other questions/concerns, please email{" "}
-                  <ExternalLink href="mailto: uaem@ssmu.ca">
-                    uaem@ssmu.ca
-                  </ExternalLink>
-                  .
-                </i>
-              </p>
             </FaqItem>
+            <p className="mb-3 leading-10">
+              <i>
+                If you have any other questions/concerns, please email{" "}
+                <ExternalLink href="mailto: uaem@ssmu.ca">
+                  uaem@ssmu.ca
+                </ExternalLink>
+                .
+              </i>
+            </p>
           </dl>
         </div>
       </div>
