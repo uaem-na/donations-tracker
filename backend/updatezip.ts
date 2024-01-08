1733fce6ded1e23f4a1f52d5bbdce5559af3ee8a
ryan <shjryan@users.noreply.github.com>
2024-01-08 00:43:09 -0500

    🔨 refactor(tools): formatting and fn param

--- backend/updatezip.ts
+++ backend/updatezip.ts
@@ -1,43 +1,44 @@
-import { MongoClient, ObjectId } from 'mongodb';
+import { Db, MongoClient, ObjectId } from "mongodb";
 
-async function updateZipCodes() {
-  const connectionString = 'mongodb://127.0.0.1:27017/dev';
+const DEFAULT_CONNECTION_STRING = "mongodb://127.0.0.1:27017/dev";
+
+async function updateZipCodes(connectionString: string) {
   const client = new MongoClient(connectionString);
-  
+
   try {
     client.connect();
 
-    const database = client.db('dev');
-    await updateCollection(database, 'users');
-    await updateCollection(database, 'posts');
+    const database = client.db("dev");
+    await updateCollection(database, "users");
+    await updateCollection(database, "posts");
 
-    console.log('Zip codes within location field updated successfully!');
+    console.log("Zip codes within location field updated successfully!");
   } finally {
     await client.close();
   }
 }
 
-async function updateCollection(database: any, collectionName: string) {
-    const collection = database.collection(collectionName);
-    const documents = await collection.find().toArray();
+async function updateCollection(database: Db, collectionName: string) {
+  const collection = database.collection(collectionName);
+  const documents = await collection.find().toArray();
 
-    for (const document of documents) {
-      const location = document.location;
-      if (location) {
-        // Update the zip code within the "location" field
-        location.postalCode = updateZipCodeFormat(location.postalCode);
+  for (const document of documents) {
+    const location = document.location;
+    if (location) {
+      // Update the zip code within the "location" field
+      location.postalCode = updateZipCodeFormat(location.postalCode);
 
-        // Update the document in the collection
-        await collection.updateOne(
-          { _id: new ObjectId(document._id) },
-          { $set: { location } }
-        );
-      }
+      // Update the document in the collection
+      await collection.updateOne(
+        { _id: new ObjectId(document._id) },
+        { $set: { location } }
+      );
     }
+  }
 }
 
 function updateZipCodeFormat(currentZipCode: string): string {
-  return currentZipCode.replace(/ /g, '').toUpperCase();
+  return currentZipCode.replace(/ /g, "").toUpperCase();
 }
 
-updateZipCodes();
+updateZipCodes(DEFAULT_CONNECTION_STRING);
