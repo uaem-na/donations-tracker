11c492970c7738e698c15248588a624f96dc528b
gülce <115143411+gcc4p@users.noreply.github.com>
2024-04-03 19:00:47 -0400

    Fixed everything and also internal server error that occurred with Delete button

--- frontend/src/components/Posts/PostDetails.tsx
+++ frontend/src/components/Posts/PostDetails.tsx
@@ -17,6 +17,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { CreateReportFormDialog } from "@pages/Admin/components/CreateReportFormDialog";
 import { DialogClose } from "@radix-ui/react-dialog";
 import {
+  useApprovePostAdminMutation,
   useDeletePostMutation,
   useGetPostQuery,
   useGetSessionQuery,
@@ -32,6 +33,7 @@ interface PostDetailsProps {
   id: string;
   onError: (err) => void;
   redirectOnDelete?: boolean;
+  redirectOnApprove?: boolean;
   hideEditDelete?: boolean;
   hideReportButton?: boolean;
   hideApproveButton?: boolean; // ADDED
@@ -41,6 +43,7 @@ export const PostDetails = ({
   id,
   onError,
   redirectOnDelete = true,
+  redirectOnApprove = true, // ADDED
   hideEditDelete = false,
   hideReportButton = false,
   hideApproveButton = false, // ADDED
@@ -50,6 +53,9 @@ export const PostDetails = ({
   const navigate = useNavigate();
 
   const [serverMessage, setServerMessage] = useState();
+
+  const [approvePostApi, {isSuccess: isApproveSuccess, error: approveError}] =
+  useApprovePostAdminMutation();
 
   const [deletePostApi, { isSuccess: isDeleteSuccess, error: deleteError }] =
     useDeletePostMutation();
@@ -75,12 +81,20 @@ export const PostDetails = ({
     deletePostApi({ id: post.id });
   };
 
+  const onApprove = async () => {
+    if (!post?.id) {
+      onError({ status: 500, message: "Post ID must be available" });
+      return;
+    }
+    approvePostApi({ postId: post.id });
+  }
+
   // handle successful requests
   useEffect(() => {
-    if (isDeleteSuccess && redirectOnDelete) {
-      navigate(`/posts/list`);
+    if ((isDeleteSuccess && redirectOnDelete) || (isApproveSuccess && redirectOnApprove)) {
+      navigate(`/posts`);
     }
-  }, [isDeleteSuccess]);
+  }, [isDeleteSuccess, isApproveSuccess]);
 
   // handle server error message
   useEffect(() => {
@@ -258,7 +272,7 @@ export const PostDetails = ({
                             intent="secondary"
                             className="flex gap-1.5 justify-center items-center"
                           >
-                            <FontAwesomeIcon icon={faCancel} />
+                            <FontAwesomeIcon icon={faCancel} /> 
                             {t("cancel")}
                           </Button>
                         </DialogClose>
@@ -273,10 +287,11 @@ export const PostDetails = ({
           )}
           {!hideApproveButton &&
             currentSession &&
-            currentSession.role.includes(UserRole.ADMIN) && (
-        <Button
+            currentSession.role.includes(UserRole.ADMIN) && post.item.category.includes("other") && (
+        <Button 
             intent="primary"
             className="flex gap-1.5 justify-center items-center"
+            onClick={onApprove}
           >
           Approve
         </Button>)}
